0d14de6ca50043e73e6f8fc8341df0006aae45a9
BearBuster <frunze.dimka@gmail.com>
2020-04-06 14:12:36 +0300

    Nothing

--- src/Dog.java
+++ src/Dog.java
@@ -1,35 +1,11 @@
 public class Dog {
+    private String name;
     private int age;
     private int weight;
-    private String name;
 
-    public Dog(int age, int weight, String name) {
+    public Dog(String name, int age, int weight) {
+        this.name = name;
         this.age = age;
         this.weight = weight;
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public String getName() {
-        return name;
     }
 }
