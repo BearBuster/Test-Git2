e6977e3297c5c7f9283533ce966e11409ac1ab9a
BearBuster <frunze.dimka@gmail.com>
2020-04-06 23:14:24 +0300

    clear

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,6 @@ public class Main {
         Mercedez benz = new Mercedez(300 , 1999 , 2000f , "benz" , true , week.FRIDAY );
         benz.maxSpeed += 1;
         System.out.println(benz.maxSpeed);
-        System.out.println(benz.maxSpeed);
-        System.out.println(benz.maxSpeed);
-        System.out.println(benz.maxSpeed);
         benz.printAll();
     }
 }
