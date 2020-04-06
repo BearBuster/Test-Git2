dd012a1820a5d03f0c74141000e49392ed2d61f3
BearBuster <frunze.dimka@gmail.com>
2020-04-06 14:13:45 +0300

    Nothing

--- src/Dog.java
+++ src/Dog.java
@@ -8,4 +8,28 @@ public class Dog {
         this.age = age;
         this.weight = weight;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
