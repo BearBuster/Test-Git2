bb7d046757e85b9fa8200f31675e85147cb0cab6
BearBuster <frunze.dimka@gmail.com>
2020-04-06 22:50:12 +0300

    Added 2 class : Car , Mercedez

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Mercedez benz = new Mercedez(300 , 1999 , 2000f , "benz" , true );
+        int i = 1 + benz.maxSpeed;
+        System.out.println(i);
+        benz.printAll();
     }
 }
